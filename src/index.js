93ff0c387e805460af63c3824274f850d111455c
Carlos A. Diaz Jimenez <ca.diaz@mueblesamerica.mx>
2018-09-18 16:24:41 -0500

    Tree Shaking

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import _ from 'lodash'
+import { cube } from './math.js'
 
 function component(){
-    let elelment = document.createElement('div')
-    elelment.innerHTML = _.join(['Hello', 'webpack!'],' ')
+    let elelment = document.createElement('pre')
+    elelment.innerHTML = [
+        'Hello webpack!',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n')
     return elelment
 }
 
