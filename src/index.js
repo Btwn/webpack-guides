5c72d721aeb6d74e92df9cf34b820acd5d30926b
Carlos A. Diaz Jimenez <ca.diaz@mueblesamerica.mx>
2018-09-18 13:39:12 -0500

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import _ from 'lodash'
+import printMe from './print.js'
 
 function component(){
     let elelment = document.createElement('div')
     elelment.innerHTML = _.join(['Hello', 'webpack!'],' ')
+
+    var btn = document.createElement('button')
+    btn.innerHTML = 'Click'
+    btn.onclick = printMe
+    elelment.appendChild(btn)
+
     return elelment
 }
 
