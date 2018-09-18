c8af43421bf2948bd9225f7b1a31a8479dfdeddb
Carlos A. Diaz Jimenez <ca.diaz@mueblesamerica.mx>
2018-09-18 12:59:50 -0500

    Assets Management

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import _ from 'lodash'
+import './style.css'
+import Icon from './icon.png'
+import Data from './data.xml'
 
 function component(){
     let elelment = document.createElement('div')
     elelment.innerHTML = _.join(['Hello', 'webpack!'],' ')
+    elelment.classList.add('hello')
+    var myIcon = new Image()
+    myIcon.src = Icon
+    elelment.appendChild(myIcon)
+    console.log(Data)
     return elelment
 }
 
