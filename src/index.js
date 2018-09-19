c3271a8e4f7a73b1438670d8ddd0319bf1f7bc54
Carlos A. Diaz <carlos4diazj@hotmail.com>
2018-09-18 22:29:53 -0500

    Lazy Loading

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-async function getComponent(){
+import _ from 'lodash'
+
+function component(){
     let elelment = document.createElement('div')
-    const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash')
+    let button = document.createElement('button')
+    button.innerHTML = 'Click'
+    let br = document.createElement('br')
     elelment.innerHTML = _.join(['Hello', 'webpack!'],' ')
+    elelment.appendChild(br)
+    elelment.appendChild(button)
+
+    button.onclick = e => import('./print.js').then(module => {
+        let print = module.default
+        print()
+    })
     return elelment
 }
 
-getComponent().then(component => {
-    document.body.appendChild(component())
-})
+document.body.appendChild(component())
